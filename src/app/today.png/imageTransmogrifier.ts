aad93f29d5173095f8d17b6fff76e5efd30c2b14
Iikku Mattila <iikku@iikku.org>
2025-10-25 09:42:15 +0300

    Separate route from the image transmogrification

---
+++ src/app/today.png/imageTransmogrifier.ts
@@ -0,0 +1,66 @@
+import { log } from "console";
+import { join } from "path";
+import { tmpdir } from "os";
+import { readFile, writeFile } from "fs/promises";
+import { execFile } from "child_process";
+
+/**
+ * Makes the image dithered grayscale and 800x480 resized.
+ */
+export const transmogrify = async (image: string) => {
+  if (image) {
+    // Write the file to the filesystem for black and white conversion with ImageMagick
+    const imgBuffer = Buffer.from(image, "base64")
+
+    const inputFile = join(tmpdir(), "in.png")
+    const outputFile = join(tmpdir(), "out.png")
+
+    await writeFile(inputFile, imgBuffer);
+
+    // See https://docs.usetrmnl.com/go/imagemagick-guide for details of the conversion
+
+    const conversionToTwoBitImage = true;
+    if (!conversionToTwoBitImage) {
+      // convert to 1 bit
+      await new Promise<void>((resolve, reject) =>
+        execFile(
+          "magick",
+          [inputFile, "-resize", "800x480", "-gravity", "center", "-extent", "800x480", "-background", "white", "-monochrome", "-colors", "2", "-depth", "1", "-strip", "png:" + outputFile],
+          (err) =>
+            err ? reject(err) : resolve()
+        )
+      )
+    }
+    else {
+      // convert to 2 bit
+      const colormap = join(tmpdir(), "colormap-2bit.png")
+
+      await new Promise<void>((resolve, reject) =>
+        execFile(
+          "magick",
+          ["-size", "4x1", "xc:#000000", "xc:#555555", "xc:#aaaaaa", "xc:#ffffff", "+append", "-type", "Palette", colormap],
+          (err) =>
+            err ? reject(err) : resolve()
+        )
+      )
+
+      await new Promise<void>((resolve, reject) =>
+        execFile(
+          "magick",
+          [inputFile, "-resize", "800x480", "-gravity", "center", "-extent", "800x480", "-background", "white", "-dither", "FloydSteinberg", "-remap", colormap, "-define", "png:bit-depth=2", "-define", "png:color-type=0", outputFile],
+          (err) =>
+            err ? reject(err) : resolve()
+        )
+      )
+    }
+
+    // Read the converted image from the filesystem and send in the response
+    var blackAndWhiteVersion = await readFile(outputFile);
+
+    log("Image transmogrified into grayscale");
+    return blackAndWhiteVersion;
+  } else {
+    log("Can't return the image 'cause there is no image");
+    return null;
+  }
+}
--- src/app/today.png/route.ts
+++ src/app/today.png/route.ts
@@ -1,72 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 import { fetchImage } from "../imageCache";
 import { log } from "console";
-import { join } from "path";
-import { tmpdir } from "os";
-import { readFile, writeFile } from "fs/promises";
-import { execFile } from "child_process";
+import { transmogrify } from "./imageTransmogrifier"
 
 export const GET = async (
   req: NextRequest,
   route: {},
 ) => {
   const generatedImage = await fetchImage();
+  const suitedForTRMNL = await transmogrify(generatedImage);
 
-  if (generatedImage) {
-    // Write the file to the filesystem for black and white conversion with ImageMagick
-    const imgBuffer = Buffer.from(generatedImage, "base64")
-
-    const inputFile = join(tmpdir(), "in.png")
-    const outputFile = join(tmpdir(), "out.png")
-
-    await writeFile(inputFile, imgBuffer);
-
-    // See https://docs.usetrmnl.com/go/imagemagick-guide for details of the conversion
-
-    const conversionToTwoBitImage = true;
-    if (!conversionToTwoBitImage) {
-      // convert to 1 bit
-      await new Promise<void>((resolve, reject) =>
-        execFile(
-          "magick",
-          [inputFile, "-resize", "800x480", "-gravity", "center", "-extent", "800x480", "-background", "white", "-monochrome", "-colors", "2", "-depth", "1", "-strip", "png:" + outputFile],
-          (err) =>
-            err ? reject(err) : resolve()
-        )
-      )
-    }
-    else {
-      // convert to 2 bit
-      const colormap = join(tmpdir(), "colormap-2bit.png")
-
-      await new Promise<void>((resolve, reject) =>
-        execFile(
-          "magick",
-          ["-size", "4x1", "xc:#000000", "xc:#555555", "xc:#aaaaaa", "xc:#ffffff", "+append", "-type", "Palette", colormap],
-          (err) =>
-            err ? reject(err) : resolve()
-        )
-      )
-
-      await new Promise<void>((resolve, reject) =>
-        execFile(
-          "magick",
-          [inputFile, "-resize", "800x480", "-gravity", "center", "-extent", "800x480", "-background", "white", "-dither", "FloydSteinberg", "-remap", colormap, "-define", "png:bit-depth=2", "-define", "png:color-type=0", outputFile],
-          (err) =>
-            err ? reject(err) : resolve()
-        )
-      )
-    }
-
-    // Read the converted image from the filesystem and send in the response
-    var blackAndWhiteVersion = await readFile(outputFile);
-
+  if (suitedForTRMNL) {
     return new NextResponse(
-      blackAndWhiteVersion,
+      suitedForTRMNL,
       {
         status: 200,
         headers: {
-          "Content-Length": blackAndWhiteVersion.length.toString(),
+          "Content-Length": suitedForTRMNL.length.toString(),
           'Content-Type': "image/png"
         }
       }
